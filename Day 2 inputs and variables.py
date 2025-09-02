190b210138c8765b7e1a0c86fd295de9a38b3924
UkrPatrick <patrick.longfield@yahoo.com>
2025-09-02 14:26:45 +0300

    Day 2 inputs and variables new script

---
+++ Day 2 inputs and variables.py
@@ -0,0 +1,8 @@
+#practising using inputs and variables 
+name = input("What is your name? ")
+age = input("How old are you? ")
+grade = input("What grade are you in? ")
+age_at_7_grade = 13
+if grade == "7": print (name, "congratulations you are in the right class")
+else: print (name, "you are not in the right class for your age")
+
