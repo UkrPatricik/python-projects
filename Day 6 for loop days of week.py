7f4b5767c3df4c71ababdff9cf4dc192e2d4ae42
UkrPatrick <patrick.longfield@yahoo.com>
2025-09-06 14:34:20 +0300

    Day 6 for loop with break

---
+++ Day 6 for loop days of week.py
@@ -0,0 +1,6 @@
+#loop using days of week
+days_of_week = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+for d in days_of_week:
+    if d == "Wednesday": break 
+    print(d) 
+#break statement will stop the loop when it reaches Wednesday
