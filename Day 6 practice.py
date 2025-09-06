e887347a21b6ea9bbca57e17f60c14032db5faee
UkrPatrick <patrick.longfield@yahoo.com>
2025-09-06 14:03:28 +0300

    Day 6 practice with def

---
+++ Day 6 practice.py
@@ -0,0 +1,11 @@
+def print_Patrick():
+    text = "Patrick is a starfish who lives under the sea."
+    print (text)
+print_Patrick()
+def print_Spongebob():
+    text = "Spongebob is a yellow sponge who lives in a pineapple."
+    print (text)
+print_Spongebob() 
+def print_Squidward(text):
+    print (text)
+print_Squidward("Squidward is an octopus who lives in a moai.")  
