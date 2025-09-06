8c475c8ccb24a640976188485a4afcb7766b0fc4
UkrPatrick <patrick.longfield@yahoo.com>
2025-09-06 14:36:29 +0300

    Day 6 for loop with continue

---
+++ Day 6 for loop days of week with  continue.py
@@ -0,0 +1,6 @@
+#loop using days of week
+days_of_week = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+for d in days_of_week:
+    if d == "Wednesday": continue
+    print(d) 
+#continue statement will skip the current iteration when it reaches Wednesday
